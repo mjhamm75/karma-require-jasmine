ab8de08db74aa94598cfadc6e9e5575d84e17075
Jason Hamm <jhamm.business@gmail.com>
2013-08-05 11:19:29 -0400

    Add switch logic to use same SpecRunner for karma or karma/jasmine together

--- js/models/Patient.js
+++ js/models/Patient.js
@@ -1,7 +1,8 @@
 define(function(require) {
-  var $ = require('jquery');
-  var _ = require('underscore');
   var Backbone = require('backbone');
 
-  return Backbone.Model.extend();
+  return Backbone.Model.extend({
+
+
+  });
 });
--- test/spec/models/PatientSpec.js
+++ test/spec/models/PatientSpec.js
@@ -3,6 +3,7 @@ require(['models/Patient'], function(Patient) {
       it('cannot be null', function () {
         var patient = new Patient();
           expect(patient).not.toBeUndefined();
+          // expect(3).toBe(2);
       });
   });
 });
