cee306ccbbdbc8a74bdc5ff336e5c25e08b79e1e
Jason Hamm <jhamm.business@gmail.com>
2013-08-07 05:37:42 -0400

    SpecRunner now has only one config piece

--- test/SpecRunner.js
+++ test/SpecRunner.js
@@ -1,6 +1,14 @@
-require.config({
-  baseUrl: "../js/",
-  urlArgs: 'cb=' + Math.random(),
+var tests = [];
+for (var file in window.__karma__.files) {
+    if (/Spec\.js$/.test(file)) {
+        tests.push(file);
+    }
+}
+
+requirejs.config({
+    // Karma serves files from '/base'
+  baseUrl: '/base/js',
+  // urlArgs: 'cb=' + Math.random(),
   paths: {
     jquery: 'lib/jquery-1.8.0',
     underscore: 'lib/underscore-1.4.2',
@@ -29,76 +37,10 @@ require.config({
       deps: ['jasmine'],
       exports: 'jasmine'
     }
-  }
+  },
+    // ask Require.js to load these files (all our tests)
+    deps: tests,
+
+    // start test run, once Require.js is done
+    callback: window.__karma__.start
 });
-
-
-if(this.__karma__) {
-  var tests = [];
-  for (var file in window.__karma__.files) {
-      if (/Spec\.js$/.test(file)) {
-          tests.push(file);
-      }
-  }
-
-  requirejs.config({
-      // Karma serves files from '/base'
-      baseUrl: '/base/js',
-
-      paths: {
-          'jquery': 'lib/jquery',
-          'underscore': 'lib/underscore',
-          'backbone': 'lib/backbone'
-      },
-
-      shim: {
-          'underscore': {
-              exports: '_'
-          }
-      },
-
-      // ask Require.js to load these files (all our tests)
-      deps: tests,
-
-      // start test run, once Require.js is done
-      callback: window.__karma__.start
-  });
-} else {
-  window.store = "TestStore"; // override local storage store name - for testing
-
-  require(['underscore', 'jquery', 'jasmine-html'], function(_, $, jasmine){
-
-    var jasmineEnv = jasmine.getEnv();
-    jasmineEnv.updateInterval = 1000;
-
-    var htmlReporter = new jasmine.HtmlReporter();
-
-    jasmineEnv.addReporter(htmlReporter);
-
-    jasmineEnv.specFilter = function(spec) {
-      return htmlReporter.specFilter(spec);
-    };
-
-    var specs = [];
-
-    specs.push('spec/models/TodoSpec');
-    specs.push('spec/models/PatientSpec');
-    specs.push('spec/views/ClearCompletedSpec');
-    specs.push('spec/views/CountViewSpec');
-    specs.push('spec/views/FooterViewSpec');
-    specs.push('spec/views/MarkAllSpec');
-    specs.push('spec/views/NewTaskSpec');
-    specs.push('spec/views/TaskListSpec');
-    specs.push('spec/views/task/TaskViewSpec');
-    specs.push('spec/views/task/ViewTaskViewSpec');
-    specs.push('spec/views/task/EditTaskViewSpec');
-
-
-    $(function(){
-      require(specs, function(){
-        jasmineEnv.execute();
-      });
-    });
-
-  });
-}
--- test/spec/models/PatientSpec.js
+++ test/spec/models/PatientSpec.js
@@ -1,9 +1,12 @@
-require(['models/Patient'], function(Patient) {
+require(['models/Patient', 'jquery'], function(Patient, $) {
   describe('Patient', function () {
       it('cannot be null', function () {
         var patient = new Patient();
           expect(patient).not.toBeUndefined();
           // expect(3).toBe(2);
       });
+      it('cannot be null', function() {
+        expect($).not.toBeUndefined();
+      })
   });
 });
