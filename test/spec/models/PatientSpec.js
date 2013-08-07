9a78d2ea9e522da35998d78b5e45f7472c6d3e5e
Jason Hamm <jhamm.business@gmail.com>
2013-08-07 15:18:56 -0400

    New Spec file

--- test/spec/models/PatientSpec.js
+++ test/spec/models/PatientSpec.js
@@ -1,12 +1,15 @@
-require(['models/Patient', 'jquery'], function(Patient, $) {
-  describe('Patient', function () {
-      it('cannot be null', function () {
-        var patient = new Patient();
-          expect(patient).not.toBeUndefined();
-          // expect(3).toBe(2);
-      });
-      it('cannot be null', function() {
-        expect($).not.toBeUndefined();
-      })
+define(['models/Patient'], function(Patient) {
+  var patient;
+  describe("Patient Model", function() {
+    beforeEach(function() {
+    patient = new Patient();
+    });
+    it("should be defined", function() {
+      expect(patient).toBeDefined();
+    });
+    it("should be a Backbone model", function() {
+      expect(_.isFunction(patient.get)).toBe(true);
+      expect(_.isFunction(patient.set)).toBe(true);
+    })
   });
 });
